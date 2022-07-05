c35006f14083557b20fbee92605c5c85920339bf
Robby Pambudi <robby.pambudi10@gmail.com>
2022-07-05 14:20:19 +0700

    feat: add form pemesanan

--- resources/views/formpemesanan.blade.php
+++ resources/views/formpemesanan.blade.php
@@ -3,20 +3,27 @@
 @section('menu', 'Form Pemesanan')
 @section('content')
 
+
 <main class="w-full h-max">
     <div class="flex flex-row mt-20 ml-20 justify-between">
         <form action="/formpemesanan" method="POST" class="flex flex-col gap-y-3 p-6">
             <h3 class="font-bold text-4xl">Form Pemesanan </h3>
             <label class="mt-5 text-base">Nama Lenkap</label>
-            <input class="w-[412px] h-[43px] rounded-xl p-2" id="name" name="name" required>
+            <input class="w-[412px] h-[43px] rounded-xl p-2" id="name" name="name" value="{{$akun->name}}" disabled>
             <label class="mt-2 text-base">Email</label>
-            <input class="w-[412px] h-[43px] rounded-xl p-2" id="email" name="email" type="email" required>
+            <input class="w-[412px] h-[43px] rounded-xl p-2" id="email" name="email" type="email"
+                value="{{$akun->email}}" disabled>
             <label class="mt-2 text-base">Alamat</label>
             <input class="w-[412px] h-[43px] rounded-xl p-2" id="alamat" name="alamat" required>
             <label class="mt-2 text-base">No Handphone</label>
-            <input class="w-[412px] h-[43px] rounded-xl p-2" id="nohp" name="nohp" required>
+            <input class="w-[412px] h-[43px] rounded-xl p-2" id="nohp" name="nohp" value="{{$akun->notelp}}" disabled>
             <label class="mt-2 text-base">Paket yang dipilih</label>
-            <input class="w-[412px] h-[43px] rounded-xl p-2">
+            <select id="paket" name="paket" class="w-[412px] h-[43px] rounded-xl bg-white p-2">
+                <option>-- Paket ---</option>
+                @foreach ($paket as $item)
+                <option value="{{$item->id_paket}}"> {{$item->nama_paketlayanan}}</option>
+                @endforeach
+            </select>
             <button type="submit"
                 class="w-[168px] text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Pesan</button>
         </form>
@@ -24,5 +31,8 @@
             <img src="https://i.ibb.co/rmZ98Xd/image-removebg-preview.png" alt="Yeah" class="mr-20 mt-20" />
         </div>
     </div>
+    {{-- @foreach ($paket as $item)
+    {{$item->nama_paketlayanan}}
+    @endforeach --}}
 </main>
 @endsection
