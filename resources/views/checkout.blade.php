0d93ed08497d359c872cd5b5ad346fdeed7dd4b5
Robby Pambudi <robby.pambudi10@gmail.com>
2022-07-05 18:59:24 +0700

    feat: add abort checkout

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -90,7 +90,7 @@
             <div class="flex justify-end flex-row">
                 <button type="submit"
                     class="w-[168px] text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Pesan</button>
-                <a type="submit"
+                <a href="gajadi/{{$idpesanan}}" type="submit"
                     class="w-[168px] text-center text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Ga
                     Jadi</a>
         </form>
